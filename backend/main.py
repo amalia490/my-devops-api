1dcc4a745aea106177c1d0985173179028518ffa
Riclea Amalia <riclea.amalia@gmail.com>
2026-07-09 23:27:43 +0300

    add dataloader for a nested query resolving n + 1 problem and updated the test

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,6 @@
 from contextlib import asynccontextmanager
-
 import strawberry
-
+from strawberry.dataloader import DataLoader
 from fastapi import FastAPI, Depends, Request, WebSocket, BackgroundTasks
 from strawberry.fastapi import GraphQLRouter
 from fastapi import FastAPI
@@ -9,7 +8,10 @@ from db import engine
 from models import User, Service, Incident
 from sqlmodel import Session, SQLModel
 from schema import schema
-
+from sqlmodel import select
+from schema import IncidentModel
+from typing import List 
+from collections import defaultdict 
 def createDBtables():
     SQLModel.metadata.create_all(engine)
 
@@ -22,9 +24,22 @@ def get_session():
     with Session(engine) as session:
         yield session
 
+#the dataloader part
+
 async def get_context(session: Session = Depends(get_session)):
+    async def load_incidents_by_service(keys: List[int]) -> List[List[IncidentModel]]:
+        statement = select(IncidentModel).where(IncidentModel.service_id.in_(keys))
+        incidents = session.exec(statement).all()
+        incidents_group = defaultdict(list)
+        for incident in incidents:
+            incidents_group[incident.service_id].append(incident)
+            
+        return [incidents_group[key] for key in keys]
+
+
     return {
         "session": session,
+        "incidents_loader": DataLoader(load_fn=load_incidents_by_service)
     }
 
 app = FastAPI(lifespan=lifespan)
